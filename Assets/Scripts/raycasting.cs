7c3e4ec346b9052fd053a5e20c51979673ffb5dd
John Allen <valerio.allen24@gmail.com>
2021-10-04 15:10:22 +1300

    Project initialized + raycasting
    set up project and raycasting need set up health and damage for cube

---
+++ Assets/Scripts/raycasting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycasting : MonoBehaviour
+{
+    public LayerMask mask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            RaycastHit hit;
+            Debug.DrawRay(Vector3.zero, transform.forward * 10, Color.green);
+            if (Physics.Raycast(Vector3.zero, transform.forward, out hit, 10, mask))
+            {
+                gameObject
+            }
+        }
+    }
+}
