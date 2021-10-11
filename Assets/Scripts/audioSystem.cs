33dd66530dc6dbc1347c4d97348457c33bb8438c
John Allen <valerio.allen24@gmail.com>
2021-10-11 15:20:52 +1300

    Audio and Post Processing,
    Audio stuff and how to post process

---
+++ Assets/Scripts/audioSystem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioSystem : MonoBehaviour
+{
+    AudioSource ASource = null;
+
+    public float timer = 0f;
+    public float timermx = .5f;
+    public AudioClip[] aClips = null; // array for the clips
+    // Start is called before the first frame update
+    void Start()
+    {
+        ASource = GetComponent<AudioSource>();
+        timer = timermx;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int aIndex = Random.Range(0, aClips.Length);// random.range excludes the max value
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            timer += timermx;
+            ASource.clip = aClips[aIndex];
+            ASource.Play();
+            /* PlayAudio(aClips[aIndex]);*/
+        }
+    }
+    void PlayAudio(AudioClip clip)
+    {
+        ASource.PlayOneShot(clip);
+    }
+}
---
+++ Assets/Scripts/rotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    public int speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localRotation = transform.localRotation * Quaternion.Euler(0, speed * Time.deltaTime, 0);
+        if (Input.GetKey("space"))
+        {
+            
+            transform.localRotation = transform.localRotation * Quaternion.Euler(0, speed * Time.deltaTime, 0);
+        }
+    }
+}
